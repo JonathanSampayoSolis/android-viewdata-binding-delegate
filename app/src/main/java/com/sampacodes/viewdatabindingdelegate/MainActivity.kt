f87990d5db6e79166c0553213a49a70f03e971a6
Jonathan Sampayo <sampayo_solis_jonathan@hotmail.com>
2021-06-20 23:01:13 -0500

    drawer in app set up finished up

--- app/src/main/java/com/sampacodes/viewdatabindingdelegate/MainActivity.kt
+++ app/src/main/java/com/sampacodes/viewdatabindingdelegate/MainActivity.kt
@@ -1,14 +1,50 @@
 package com.sampacodes.viewdatabindingdelegate
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.sampacodes.viewdatabindingdelegate.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val navController: NavController by lazy {
+        supportFragmentManager.findFragmentById(R.id.nav_host_fragment).let {
+            (it as NavHostFragment).navController
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(binding.root)
 
-        setContentView(R.layout.activity_main)
+        val appBarConfig = AppBarConfiguration(
+            setOf(R.id.viewBindingFragment, R.id.dataBindingFragment),
+            binding.drawerLayout,
+        )
+        setupActionBarWithNavController(navController, appBarConfig)
+
+        binding.navigationView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (item.itemId == android.R.id.home) {
+            binding.drawerLayout.openDrawer(GravityCompat.START)
+            true
+        } else false
     }
 
 }
